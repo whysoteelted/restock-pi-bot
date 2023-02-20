d7555df319ee3b25d1b4408877d48b5bfec0419a
Alessandro Caridi <alessandro@fluidware.it>
2023-02-21 00:48:08 +0100

    Remove some console.log

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ const BOT_TOKEN = process.env.BOT_API_TOKEN || ''
 const CHANNEL_ID = process.env.CHANNEL_ID || ''
 
 const sendTelegramMessage = (msg) => {
-  console.log('bot', BOT_TOKEN, 'cid', CHANNEL_ID, 'msg', msg);
   fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHANNEL_ID}&text=${msg}`, {method: 'POST'})
     .then((response) => response.json())
     .then((data) => console.log(data));
@@ -20,7 +19,6 @@ const sendTelegramMessage = (msg) => {
 
 const getFeed = async () => {
   const feed = await parser.parseURL(RSS_FEED);
-  console.log(feed);
   feed.items.forEach(item => {
     sendTelegramMessage(item.title + ': ' + item.link)
   });
