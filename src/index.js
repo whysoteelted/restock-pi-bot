308a87c1edfce2a88c83b47bb2088a4e4ac40d65
Alessandro Caridi <alessandro@fluidware.it>
2023-03-07 10:23:59 +0100

    Rewrite bot using telegraf package, remove node-fetch package

--- src/index.js
+++ src/index.js
@@ -1,29 +1,47 @@
 import Parser from "rss-parser";
 import dotenv from "dotenv";
-import fetch from "node-fetch";
+import { Telegraf } from "telegraf";
 
 dotenv.config();
-const parser = new Parser();
 
 const RSS_FEED = process.env.RSS_FEED || 'https://rpilocator.com/feed/?country=IT';
 const REQUEST_INTERVAL = process.env.REQUEST_INTERVAL || 300000;
+const BOT_TOKEN = process.env.BOT_API_TOKEN || '';
+const CHANNEL_ID = process.env.CHANNEL_ID || '';
 
-const BOT_TOKEN = process.env.BOT_API_TOKEN || ''
-const CHANNEL_ID = process.env.CHANNEL_ID || ''
+const parser = new Parser();
+const bot = new Telegraf(BOT_TOKEN);
+let lastGuid = '';
+let feedItems = [];
 
-const sendTelegramMessage = (msg) => {
-  fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHANNEL_ID}&text=${msg}`, {method: 'POST'})
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+const sendTelegramMessage =async (msg) => {
+  await bot.telegram.sendMessage(CHANNEL_ID, msg);
 }
 
-const getFeedAndSend = async () => {
+const checkFeed = async () => {
   const feed = await parser.parseURL(RSS_FEED);
-  feed.items.forEach(item => {
-    sendTelegramMessage(item.title + ': ' + item.link)
-  });
+  for (const item of feed.items) {
+    if (item.guid === lastGuid) {
+      break;
+    }
+    feedItems.push(item);
+  }
+  return feedItems;
+};
+
+const bulkSendTelegramMessages = async (filteredFeedItems) => {
+  const messagesToBeSent = filteredFeedItems.slice().reverse();
+  if (messagesToBeSent.length > 0) {
+    for (let i = 0; i < messagesToBeSent.length; i++) {
+      const item = messagesToBeSent[i];
+      if (i === messagesToBeSent.length - 1) { lastGuid = item.guid };
+      setTimeout(async () => sendTelegramMessage(item.title + ': ' + item.link), 2000 * i);
+    }
+  }
 }
 
-setInterval(() => {
-  getFeedAndSend();
+setInterval(async () => {
+  const newFeed = await checkFeed();
+  bulkSendTelegramMessages(newFeed);
+  feedItems = [];
 }, REQUEST_INTERVAL);
